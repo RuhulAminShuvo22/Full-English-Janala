baeb81849076a12124fcd3ccad3ee6acc62338f6
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-07 04:14:40 +0600

    java script section done

--- script/index.js
+++ script/index.js
@@ -159,3 +159,46 @@ const displayLevelWord = (words) => {
   });
   manageSpinner(false);
 };
+
+const displayLesson = (lessons) => {
+  //   1. get the container & empty
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  //   2. get into evey lessons
+  for (let lesson of lessons) {
+    //         3. create Element
+    // console.log(lesson);
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+                 <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})"  class="btn btn-outline btn-primary lesson-btn">
+                 <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+                  </button>
+    `;
+
+    //         4. append into container
+
+    levelContainer.append(btnDiv);
+  }
+};
+
+loadLessons();
+
+document.getElementById("btn-search").addEventListener("click", () => {
+  removeActive();
+  const input = document.getElementById("input-search");
+  const searchValue = input.value.trim().toLowerCase();
+  console.log(searchValue);
+
+  fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res) => res.json())
+    .then((data) => {
+      const allWords = data.data;
+      console.log(allWords);
+      const filterWords = allWords.filter((word) =>
+        word.word.toLowerCase().includes(searchValue)
+      );
+
+      displayLevelWord(filterWords);
+    });
+});
